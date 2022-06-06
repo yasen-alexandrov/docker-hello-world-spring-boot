8d910d9d176cf544e1dd48d03925d873a6012901
Yasen Aleksandrov <y.aleksandrov@prosyst.bg>
2022-06-06 17:56:16 +0300

    changed welcome message.

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello World\n";
+    return "Hello World! This s a containerized Spring Boot app!\n";
     }
     
 }
